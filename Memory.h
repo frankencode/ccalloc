e2b61a19d29958319be3037e4510d3a4cef8ac7a
Frank Mertens <frank@cyblogic.de>
2016-07-11 00:24:58 +0200

    Initial commit

---
+++ Memory.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2007-2016 Frank Mertens.
+ *
+ * Distribution and use is allowed under the terms of the zlib license
+ * (see LICENSE-zlib).
+ *
+ */
+
+#pragma once
+
+#include <sys/types.h>
+
+namespace cc {
+
+/** \brief memory allocator
+  */
+class Memory
+{
+public:
+    static void *allocate(size_t size) throw();
+    static void free(void *data) throw();
+
+private:
+    Memory();
+
+    static Memory *instance() throw();
+
+    class BucketHeader;
+
+    size_t pageSize_;
+    BucketHeader *bucket_;
+};
+
+} // namespace cc
